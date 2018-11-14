2f038260609c15e86bcd8b3cef7b9ae9948e83f8
Serafeim Papastefanos <spapas@gmail.com>
2018-11-14 14:37:37 +0200

    Change version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='python-git-info',
-    version='0.1.0',
+    version='0.2.0',
     description='Get git information repository, directly from .git',
 
     author='Serafeim Papastefanos',

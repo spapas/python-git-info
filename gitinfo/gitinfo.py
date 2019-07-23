252a6c66e5217013ad1b2384646ab21f9def507b
Serafeim Papastefanos <spapas@gmail.com>
2019-07-23 14:42:53 +0300

    Refactoring ...

--- gitinfo/gitinfo.py
+++ gitinfo/gitinfo.py
@@ -4,6 +4,7 @@ import zlib
 
 
 def find_git_dir(dir):
+    "Find the correct git dir; move upwards if .git folder is not found here"
     absdir = os.path.abspath(dir)
     gitdir = os.path.join(absdir, ".git")
     if os.path.isdir(gitdir):
@@ -14,17 +15,19 @@ def find_git_dir(dir):
         return None
     return find_git_dir(parentdir)
 
+
 def parse_commiter_line(line):
+    "Parse the commiter/author line which also contains a datetime"
     parts = line.split()
     # TODO: I'll ignore tz for now It is parts[:-1]
     unix_time = float(parts[-2])
-    commiter = ' '.join(parts[1:-2])
-    commit_time = time.strftime(
-        "%Y-%m-%d %H:%M:%S", time.localtime(unix_time)
-    )
+    commiter = " ".join(parts[1:-2])
+    commit_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(unix_time))
     return commiter, commit_time
 
-def get_git_info_dir(dir):
+
+def get_head_commit(dir):
+    "Retrieve the HEAD commit of this repo had"
     head_file = os.path.join(dir, "HEAD")
     if not os.path.isfile(head_file):
         return
@@ -46,6 +49,12 @@ def get_git_info_dir(dir):
     head_commit = None
     with open(head_ref_file, "r") as fl:
         head_commit = fl.read().strip()
+        return head_commit
+
+def get_git_info_dir(dir):
+    head_commit = get_head_commit(dir)
+    if not head_commit:
+        return
 
     head_message_folder = head_commit[:2]
     head_message_filename = head_commit[2:]
@@ -58,51 +67,47 @@ def get_git_info_dir(dir):
 
     with open(head_message_file, "rb") as fl:
         data = zlib.decompress(fl.read())
-        if not data[:6] == b'commit':
+        if not data[:6] == b"commit":
             # Not a commit object for some reason...
-            return 
+            return
         # Retrieve the null_byte_idx and start from there
-        null_byte_idx = data.index(b'\x00') + 1
+        null_byte_idx = data.index(b"\x00") + 1
         data = data[null_byte_idx:]
 
-        lines = data.decode("utf-8").split('\n')
+        lines = data.decode("utf-8").split("\n")
         reading_pgp = False
         reading_msg = False
 
-        gi = {
-            "commit": head_commit,
-            "message": ""
-        }
+        gi = {"commit": head_commit, "message": ""}
 
         for l in lines:
-            if l == '':
+            if l == "":
                 reading_pgp = False
                 reading_msg = True
-                continue 
-                
+                continue
+
             if reading_pgp == True:
                 continue
 
             if reading_msg == True:
                 gi["message"] += l
 
-            if l.startswith('tree'):
+            if l.startswith("tree"):
                 gi["tree"] = l.split()[1]
-            elif l.startswith('parent'):
+            elif l.startswith("parent"):
                 gi["parent"] = l.split()[1]
-            elif l.startswith('gpgsig'):
+            elif l.startswith("gpgsig"):
                 reading_pgp = True
-            elif l.startswith('commiter'):
+            elif l.startswith("commiter"):
                 commiter, commit_time = parse_commiter_line(l)
                 gi["commiter"] = commiter
                 gi["commit_date"] = commit_time
-            elif l.startswith('author'):
+            elif l.startswith("author"):
                 author, author_time = parse_commiter_line(l)
                 gi["author"] = author
                 gi["author_date"] = author_time
 
         gi["gitdir"] = dir
-        
 
         return gi
 
